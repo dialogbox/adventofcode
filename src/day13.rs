f55db291d5c7a729bc1267bfff40f1f691443c88
Jason Kim <dialogbox@gmail.com>
2021-12-14 08:25:34 +0900

    even better version for day13

--- src/day13.rs
+++ src/day13.rs
@@ -21,39 +21,61 @@ fn read_folding_input(filename: &str) -> std::io::Result<Vec<(char, usize)>> {
 }
 
 #[allow(dead_code)]
-fn read_coord_input(filename: &str) -> std::io::Result<Vec<Vec<bool>>> {
+fn read_coords(filename: &str) -> std::io::Result<Vec<(usize, usize)>> {
     let lines = input_lines(filename)?;
 
-    let mut coords = Vec::new();
+    let mut result = Vec::new();
 
     for l in lines {
         let l = l?;
+
         let mut xy = l.split_terminator(",");
         let x = xy.next().unwrap().parse::<usize>().unwrap();
         let y = xy.next().unwrap().parse::<usize>().unwrap();
 
-        coords.push((x, y));
-    }
-
-    let mut x_max = 0;
-    let mut y_max = 0;
-
-    for c in &coords {
-        x_max = max(x_max, c.0);
-        y_max = max(y_max, c.1);
-    }
-
-    let mut result = vec![vec![false; x_max + 1]; y_max + 1];
-
-    for (x, y) in coords {
-        result[y][x] = true;
+        result.push((x, y));
     }
 
     Ok(result)
 }
 
 #[allow(dead_code)]
-fn fold_x(mut map: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+fn read_map(filename: &str) -> std::io::Result<Vec<Vec<bool>>> {
+    let coords = read_coords(filename)?;
+    Ok(to_dense(&coords))
+}
+
+#[allow(dead_code)]
+fn fold_coords_x(mut coords: Vec<(usize, usize)>, pos: usize) -> Vec<(usize, usize)> {
+    for (x, _) in coords.iter_mut() {
+        if *x > pos {
+            *x = (pos - 1) - (*x - pos - 1);
+        }
+    }
+
+    coords.sort();
+    coords.dedup();
+
+    coords
+}
+
+#[allow(dead_code)]
+fn fold_coords_y(mut coords: Vec<(usize, usize)>, pos: usize) -> Vec<(usize, usize)> {
+    for (_, y) in coords.iter_mut() {
+        if *y > pos {
+            *y = (pos - 1) - (*y - pos - 1);
+        }
+    }
+
+    coords.sort();
+    coords.dedup();
+
+    coords
+}
+
+// naive version
+#[allow(dead_code)]
+fn fold_map_x(mut map: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     let pos = map[0].len() / 2;
     for l in map.iter_mut() {
         let mut later = l.split_off(pos + 1);
@@ -68,8 +90,9 @@ fn fold_x(mut map: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     map
 }
 
+// naive version
 #[allow(dead_code)]
-fn fold_y(map: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+fn fold_map_y(map: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     let mut prio = map;
     let mut later = prio.split_off(prio.len() / 2);
     // throw away the split line
@@ -86,7 +109,34 @@ fn fold_y(map: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
 }
 
 #[allow(dead_code)]
-fn print_status(map: &Vec<Vec<bool>>) {
+fn to_dense(coords: &Vec<(usize, usize)>) -> Vec<Vec<bool>> {
+    let mut x_max = 0;
+    let mut y_max = 0;
+
+    for (x, y) in coords {
+        x_max = max(x_max, *x);
+        y_max = max(y_max, *y);
+    }
+
+    let mut result = vec![vec![false; x_max + 1]; y_max + 1];
+
+    for (x, y) in coords {
+        result[*y][*x] = true;
+    }
+
+    result
+}
+
+#[allow(dead_code)]
+fn print_coords(coords: &Vec<(usize, usize)>) {
+    for (x, y) in coords {
+        println!("({},{})", x, y);
+    }
+    println!("--------");
+}
+
+#[allow(dead_code)]
+fn print_map(map: &Vec<Vec<bool>>) {
     for l in map {
         let t = l
             .iter()
@@ -109,9 +159,9 @@ mod tests {
 
     #[test]
     fn test_read_input() {
-        let map = read_coord_input("inputs/day13_test.txt").unwrap();
+        let map = read_map("inputs/day13_test.txt").unwrap();
 
-        print_status(&map);
+        print_map(&map);
 
         let folds = read_folding_input("inputs/day13_test_fold.txt").unwrap();
 
@@ -120,9 +170,9 @@ mod tests {
 
     #[test]
     fn test_read_real_input() {
-        let map = read_coord_input("inputs/day13.txt").unwrap();
+        let map = read_map("inputs/day13.txt").unwrap();
 
-        print_status(&map);
+        print_map(&map);
 
         let folds = read_folding_input("inputs/day13_fold.txt").unwrap();
 
@@ -131,16 +181,16 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let map = read_coord_input("inputs/day13_test.txt").unwrap();
+        let map = read_map("inputs/day13_test.txt").unwrap();
         let folds = read_folding_input("inputs/day13_test_fold.txt").unwrap();
 
         let result = if folds[0].0 == 'x' {
-            fold_x(map)
+            fold_map_x(map)
         } else {
-            fold_y(map)
+            fold_map_y(map)
         };
 
-        print_status(&result);
+        print_map(&result);
 
         let mut n = 0;
         for l in &result {
@@ -154,23 +204,47 @@ mod tests {
         assert_eq!(n, 17);
 
         let result = if folds[1].0 == 'x' {
-            fold_x(result)
+            fold_map_x(result)
         } else {
-            fold_y(result)
+            fold_map_y(result)
         };
 
-        print_status(&result);
+        print_map(&result);
+    }
+
+    #[test]
+    fn test_part1_v2() {
+        let coords = read_coords("inputs/day13_test.txt").unwrap();
+        let folds = read_folding_input("inputs/day13_test_fold.txt").unwrap();
+
+        let result = if folds[0].0 == 'x' {
+            fold_coords_x(coords, folds[0].1)
+        } else {
+            fold_coords_y(coords, folds[0].1)
+        };
+
+        print_map(&to_dense(&result));
+
+        assert_eq!(result.len(), 17);
+
+        let result = if folds[1].0 == 'x' {
+            fold_coords_x(result, folds[1].1)
+        } else {
+            fold_coords_y(result, folds[1].1)
+        };
+
+        print_map(&to_dense(&result));
     }
 
     #[test]
     fn do_part1() {
-        let map = read_coord_input("inputs/day13.txt").unwrap();
+        let map = read_map("inputs/day13.txt").unwrap();
         let folds = read_folding_input("inputs/day13_fold.txt").unwrap();
 
         let result = if folds[0].0 == 'x' {
-            fold_x(map)
+            fold_map_x(map)
         } else {
-            fold_y(map)
+            fold_map_y(map)
         };
 
         let mut n = 0;
@@ -186,20 +260,72 @@ mod tests {
     }
 
     #[test]
+    fn do_part1_v2() {
+        let coords = read_coords("inputs/day13.txt").unwrap();
+        let folds = read_folding_input("inputs/day13_fold.txt").unwrap();
+
+        let result = if folds[0].0 == 'x' {
+            fold_coords_x(coords, folds[0].1)
+        } else {
+            fold_coords_y(coords, folds[0].1)
+        };
+
+        let n = result.len();
+
+        assert_eq!(n, 802);
+    }
+
+    #[test]
     fn do_part2() {
-        let map = read_coord_input("inputs/day13.txt").unwrap();
+        let map = read_map("inputs/day13.txt").unwrap();
         let folds = read_folding_input("inputs/day13_fold.txt").unwrap();
 
         let mut result = map;
 
         for f in folds {
             result = if f.0 == 'x' {
-                fold_x(result)
+                fold_map_x(result)
             } else {
-                fold_y(result)
+                fold_map_y(result)
             };
         }
 
-        print_status(&result);
+        print_map(&result);
+    }
+
+    #[test]
+    fn do_part2_v2() {
+        let coords = read_coords("inputs/day13.txt").unwrap();
+        let folds = read_folding_input("inputs/day13_fold.txt").unwrap();
+
+        let mut result = coords;
+
+        for (axis, pos) in folds {
+            result = if axis == 'x' {
+                fold_coords_x(result, pos)
+            } else {
+                fold_coords_y(result, pos)
+            };
+        }
+
+        print_map(&to_dense(&result));
+    }
+
+    #[test]
+    fn do_part2_huge() {
+        let coords = read_coords("inputs/day13_huge.txt").unwrap();
+        let folds = read_folding_input("inputs/day13_huge_fold.txt").unwrap();
+
+        let mut result = coords;
+
+        for (axis, pos) in folds {
+            result = if axis == 'x' {
+                fold_coords_x(result, pos)
+            } else {
+                fold_coords_y(result, pos)
+            };
+        }
+
+        print_map(&to_dense(&result));
     }
 }
