247ae386e5cfc200363412b8d7e710200e5d5e30
Jason Kim <dialogbox@gmail.com>
2021-12-13 09:00:36 +0900

    day12 Rc<RefCell<_>> version

--- src/day12.rs
+++ src/day12.rs
@@ -1,5 +1,9 @@
 use super::input_lines;
-use std::collections::{HashMap, HashSet};
+use std::{
+    cell::RefCell,
+    collections::HashMap,
+    rc::{Rc, Weak},
+};
 
 #[allow(dead_code)]
 #[derive(Debug, PartialEq)]
@@ -10,17 +14,9 @@ enum Type {
     LargeCave,
 }
 
-#[allow(dead_code)]
-#[derive(Debug)]
-struct Cave {
-    cave_type: Type,
-    name: String,
-    connection: HashSet<String>,
-}
-
-impl Cave {
-    fn with_name(name: &str) -> Cave {
-        let t = match name {
+impl Type {
+    fn from_name(name: &str) -> Type {
+        match name {
             "start" => Type::Start,
             "end" => Type::End,
 
@@ -31,23 +27,43 @@ impl Cave {
                     Type::SmallCave
                 }
             }
-        };
-        Cave {
-            cave_type: t,
-            name: name.to_string(),
-            connection: HashSet::new(),
         }
     }
 }
 
 #[allow(dead_code)]
-fn read_input(filename: &str) -> std::io::Result<HashMap<String, Cave>> {
+#[derive(Debug)]
+struct Cave {
+    cave_type: Type,
+    name: String,
+    connection: Vec<Weak<RefCell<Cave>>>,
+}
+
+impl Cave {
+    fn with_name(name: &str) -> Cave {
+        let t = Type::from_name(name);
+        Cave {
+            cave_type: t,
+            name: name.to_string(),
+            connection: Vec::new(),
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn read_input(filename: &str) -> std::io::Result<HashMap<String, Rc<RefCell<Cave>>>> {
     let lines = input_lines(filename)?;
 
     let mut caves = HashMap::new();
 
-    caves.insert("start".to_string(), Cave::with_name("start"));
-    caves.insert("end".to_string(), Cave::with_name("end"));
+    caves.insert(
+        "start".to_string(),
+        Rc::new(RefCell::new(Cave::with_name("start"))),
+    );
+    caves.insert(
+        "end".to_string(),
+        Rc::new(RefCell::new(Cave::with_name("end"))),
+    );
 
     for l in lines {
         let l = l?;
@@ -56,20 +72,26 @@ fn read_input(filename: &str) -> std::io::Result<HashMap<String, Cave>> {
         let to_str = elems.next().unwrap();
 
         if !caves.contains_key(from_str) {
-            caves.insert(from_str.to_string(), Cave::with_name(from_str));
+            caves.insert(
+                from_str.to_string(),
+                Rc::new(RefCell::new(Cave::with_name(from_str))),
+            );
         }
         if !caves.contains_key(to_str) {
-            caves.insert(to_str.to_string(), Cave::with_name(to_str));
+            caves.insert(
+                to_str.to_string(),
+                Rc::new(RefCell::new(Cave::with_name(to_str))),
+            );
         }
 
-        let mut from = caves.remove(from_str).unwrap();
-        let mut to = caves.remove(to_str).unwrap();
+        let from = caves.remove(from_str).unwrap();
+        let to = caves.remove(to_str).unwrap();
 
-        from.connection.insert(to.name.to_string());
-        to.connection.insert(from.name.to_string());
+        from.borrow_mut().connection.push(Rc::downgrade(&to));
+        to.borrow_mut().connection.push(Rc::downgrade(&from));
 
-        caves.insert(from.name.clone(), from);
-        caves.insert(to.name.clone(), to);
+        caves.insert(from_str.to_string(), from);
+        caves.insert(to_str.to_string(), to);
     }
 
     Ok(caves)
@@ -77,30 +99,31 @@ fn read_input(filename: &str) -> std::io::Result<HashMap<String, Cave>> {
 
 #[allow(dead_code)]
 fn find_next_paths<'a>(
-    map: &'a HashMap<String, Cave>,
-    start: &Cave,
+    map: &'a HashMap<String, Rc<RefCell<Cave>>>,
+    start: Rc<RefCell<Cave>>,
     parent_path: &Vec<String>,
     allowed_visit_for_smallcave: usize,
 ) -> Vec<Vec<String>> {
     let mut result = Vec::new();
 
     let mut current_path = parent_path.clone();
-    current_path.push(start.name.to_string());
+    let inner_start = (*start).borrow();
+    current_path.push(inner_start.name.to_string());
 
-    if start.cave_type == Type::End {
+    if inner_start.cave_type == Type::End {
         return vec![current_path];
     }
 
-    for i in &start.connection {
-        let next = map.get(i).unwrap();
-
-        if next.cave_type == Type::Start {
+    for next in &inner_start.connection {
+        let rc_next = next.upgrade().unwrap();
+        let inner_next = (&*rc_next).borrow();
+        if inner_next.cave_type == Type::Start {
             continue;
         }
-        if next.cave_type == Type::SmallCave {
+        if inner_next.cave_type == Type::SmallCave {
             let mut n_visited_for_all = HashMap::new();
             for p in &current_path {
-                if map.get(p).unwrap().cave_type != Type::SmallCave {
+                if Type::from_name(p) != Type::SmallCave {
                     continue;
                 }
                 if !n_visited_for_all.contains_key(p) {
@@ -109,7 +132,7 @@ fn find_next_paths<'a>(
                 *n_visited_for_all.get_mut(p).unwrap() += 1;
             }
 
-            let n_visited = *n_visited_for_all.get(&next.name).unwrap_or(&0);
+            let n_visited = *n_visited_for_all.get(&inner_next.name).unwrap_or(&0);
 
             if allowed_visit_for_smallcave == 1 {
                 if n_visited == 1 {
@@ -131,7 +154,7 @@ fn find_next_paths<'a>(
 
         result.append(&mut find_next_paths(
             map,
-            next,
+            next.upgrade().unwrap(),
             &current_path,
             allowed_visit_for_smallcave,
         ));
@@ -142,12 +165,12 @@ fn find_next_paths<'a>(
 
 #[allow(dead_code)]
 fn find_paths<'a>(
-    map: &'a HashMap<String, Cave>,
+    map: &'a HashMap<String, Rc<RefCell<Cave>>>,
     allowed_visit_for_smallcave: usize,
 ) -> Vec<Vec<String>> {
     let start = map.get("start").unwrap();
 
-    find_next_paths(map, start, &Vec::new(), allowed_visit_for_smallcave)
+    find_next_paths(map, start.clone(), &Vec::new(), allowed_visit_for_smallcave)
 }
 
 #[cfg(test)]
