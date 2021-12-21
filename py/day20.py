ee8fa26c27853c3652320fc69e37ccfe6b234943
Jason Kim <dialogbox@gmail.com>
2021-12-21 10:04:45 +0900

    day20

---
+++ py/day20.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+def improve(input, background):
+  image = np.array([[1 if c == '#' else 0 for c in l] for l in input])
+
+  height, width = image.shape
+  # expend
+  nimage = np.empty(shape=(height+6, width+6))
+  nimage[:] = background
+  nimage[3:height+3, 3:width+3] = image
+
+  height, width = nimage.shape
+
+  conv = np.array([["".join(np.char.mod('%d',(nimage[y-1:y+2, x-1:x+2].reshape(1,9)[0]))) for x in range(1,width-1) ] for y in range(1,height-1)])
+
+  image = np.array([[algo[int(i,2)] for i in l] for l in conv])
+
+  return image[1:-1, 1:-1]
+
+def print_image(image):
+  print("--------------------")
+  print("\n".join(["".join(l) for l in image]))
+
+def next_background(b0, b511):
+  if b0 == '.':
+    while True:
+      yield 0
+  elif b0 == '#' and b511 == '.':
+    while True:
+      yield 0
+      yield 1
+  else:
+    yield 0
+    while True:
+      yield 1
+
+if __name__ == '__main__':
+  f = open('inputs/day20.txt', 'r')
+  algo = f.readline().strip()
+  f.readline()
+  image = np.array([[c for c in l.strip()] for l in f])
+
+  backgroud_iter = next_background(algo[0], algo[-1])
+
+  print_image(image)
+  for i in range(50):
+    image = improve(image, next(backgroud_iter))
+
+  print_image(image)
+
+  print(len(image[image == '#']))
