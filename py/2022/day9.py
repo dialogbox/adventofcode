47f59c4cd64e8f4c6b2e7270d4b89ec1ca277e04
Jason Kim <dialogbox@gmail.com>
2022-12-09 16:49:05 +0900

    Even more cleaned

--- py/2022/day9.py
+++ py/2022/day9.py
@@ -7,7 +7,26 @@ def parse_input(path):
     return [(d, int(n)) for [d, n] in [l.split(" ") for l in lines]]
 
 
-def move_one_knot(h, t):
+STEP = {
+    "U": (0, 1),
+    "D": (0, -1),
+    "R": (1, 0),
+    "L": (-1, 0),
+}
+
+
+def move_head(rope, direction, n):
+    tail_visited = set([rope[-1]])
+    (step_x, step_y) = STEP[direction]
+    for i in range(n):
+        rope[0] = (rope[0][0] + step_x, rope[0][1] + step_y)
+        fix_knots(rope)
+        tail_visited.add(rope[-1])
+
+    return (rope, tail_visited)
+
+
+def fix_one_knot(h, t):
     (hx, hy) = h
     (tx, ty) = t
 
@@ -22,66 +41,16 @@ def move_one_knot(h, t):
     return (tx + x_dir, ty + y_dir)
 
 
-def fix_rope(rope):
+def fix_knots(rope):
     for i in range(len(rope) - 1):
-        rope[i + 1] = move_one_knot(rope[i], rope[i + 1])
-
-
-def move_head_up(rope, n):
-    tail_visited = set([rope[-1]])
-    for i in range(n):
-        rope[0] = (rope[0][0], rope[0][1] + 1)
-        fix_rope(rope)
-        tail_visited.add(rope[-1])
-
-    return (rope, tail_visited)
-
-
-def move_head_down(rope, n):
-    tail_visited = set([rope[-1]])
-    for i in range(n):
-        rope[0] = (rope[0][0], rope[0][1] - 1)
-        fix_rope(rope)
-        tail_visited.add(rope[-1])
-
-    return (rope, tail_visited)
-
-
-def move_head_right(rope, n):
-    tail_visited = set([rope[-1]])
-    for i in range(n):
-        rope[0] = (rope[0][0] + 1, rope[0][1])
-        fix_rope(rope)
-        tail_visited.add(rope[-1])
-
-    return (rope, tail_visited)
-
-
-def move_head_left(rope, n):
-    tail_visited = set([rope[-1]])
-    for i in range(n):
-        rope[0] = (rope[0][0] - 1, rope[0][1])
-        fix_rope(rope)
-        tail_visited.add(rope[-1])
-
-    return (rope, tail_visited)
+        rope[i + 1] = fix_one_knot(rope[i], rope[i + 1])
 
 
 def simulate(rope, moves):
     tail_visited = set([rope[-1]])
     for m in moves:
-        if m[0] == "U":
-            (rope, new_tail_visited) = move_head_up(rope, m[1])
-            tail_visited = tail_visited.union(new_tail_visited)
-        elif m[0] == "D":
-            (rope, new_tail_visited) = move_head_down(rope, m[1])
-            tail_visited = tail_visited.union(new_tail_visited)
-        elif m[0] == "R":
-            (rope, new_tail_visited) = move_head_right(rope, m[1])
-            tail_visited = tail_visited.union(new_tail_visited)
-        elif m[0] == "L":
-            (rope, new_tail_visited) = move_head_left(rope, m[1])
-            tail_visited = tail_visited.union(new_tail_visited)
+        (rope, new_tail_visited) = move_head(rope, m[0], m[1])
+        tail_visited = tail_visited.union(new_tail_visited)
 
     return (rope, tail_visited)
 
